0acc9c6a05f22d0c73a8015b30337d94a0dd32db
JuanDaa0304 <jdvargas17@utpl.edu.ec>
2024-11-07 20:40:45 -0500

    taller5

---
+++ Proyecto_Taller5/src/Ejercicio5_AñoBisiesto.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+
+/**
+ *Año Bisiesto: Comprobar si un año ingresado es bisiesto o no. Si es bisiesto, mostrar un mensaje especial.
+ * @author Juan Vargas
+ */
+public class Ejercicio5_AñoBisiesto {
+    public static void main(String[] args) {
+        Scanner tcl = new Scanner(System.in);
+    }
+}
