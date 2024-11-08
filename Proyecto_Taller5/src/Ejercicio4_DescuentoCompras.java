184b2e85531be1d25837ded70021f5e68ff7cc7d
david02-bowie <davidalejandromerinoespinosa@gmail.com>
2024-11-08 11:37:25 -0500

    cambios taller 5t

--- Proyecto_Taller5/src/Ejercicio4_DescuentoCompras.java
+++ Proyecto_Taller5/src/Ejercicio4_DescuentoCompras.java
@@ -1,12 +1,32 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
+/**Descuento en Compras: Calcular el precio final de un producto con un descuento 
+ del 10% si el precio original es mayor de $100; de lo contrario, no hay descuento.
  *
- * @author HP
+ * @author David Merino
  */
+import java.util.Scanner;
 public class Ejercicio4_DescuentoCompras {
-    
+    public static void main(String[] args) {
+        float precio_final;
+        float descuento = 0.10f;
+        float precio_producto;
+        Scanner tcl = new Scanner(System.in);
+  //proceso
+        System.out.println("Ingrese el valor del producto");
+        precio_producto = tcl.nextFloat();
+        
+        if (precio_producto >100){
+            precio_final = precio_producto - (descuento * precio_producto);
+            System.out.println("El precio final es " + precio_final);
+        }else { 
+            precio_final = precio_producto;
+            System.out.println("El precio del producto es " + precio_final);
+            
+            
+        }
+    }
 }
+/***
+ * Ingrese el valor del producto
+   125.50
+   El precio final es 112.95
+ */
--- Proyecto_Taller5/src/Ejercicio6_CategoriaEdad.java
+++ Proyecto_Taller5/src/Ejercicio6_CategoriaEdad.java
@@ -1,12 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+/**Categoría de Edad: Clasificar a una persona en una categoría de edad
+  (niño, adolescente, adulto) en función de su edad.
+ * @author David Merino
  */
-
-/**
- *
- * @author HP
- */
+import java.util.Scanner;
 public class Ejercicio6_CategoriaEdad {
+    public static void main(String[] args) {
+        int edad;
+        Scanner tcl = new Scanner(System.in);
+    //proceso
+        System.out.println("ingrese edad");
+        edad = tcl.nextInt();
     
+        if (edad >4 && edad <12){
+                System.out.println("niño");      
+        }else if (edad >12 && edad <18){
+            System.out.println("Adolecente");   
+        }else if (edad >18 && edad <65){
+            System.out.println("Adulto");    
+        }else {
+            System.out.println("Su edad no esta dentro de los parametros");
+        }
+            
+    }
+  
 }
